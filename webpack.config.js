6e19b36fff9735cbb5aec92a6b758b655a24304a
remfath <remfath@gmail.com>
2017-06-04 22:50:54 +0800

    add build command for production

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,8 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const webpack = require('webpack');
 
-module.exports = {
+const config = {
     entry: './app/index.js',
     output: {
         path: path.resolve(__dirname, 'dist'),
@@ -21,7 +22,7 @@ module.exports = {
         ]
     },
     devServer: {
-      historyApiFallback: true
+        historyApiFallback: true
     },
     plugins: [
         new HtmlWebpackPlugin({
@@ -29,3 +30,17 @@ module.exports = {
         })
     ]
 };
+
+
+if (process.env.NODE_ENV === 'production') {
+    config.plugins.push(
+        new webpack.DefinePlugin({
+            'process.env': {
+                'NODE_ENV': JSON.stringify(process.env.NODE_ENV)
+            }
+        }),
+        new webpack.optimize.UglifyJsPlugin()
+    );
+}
+
+module.exports = config;
